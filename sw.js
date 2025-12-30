802a6863bba85903615169ff9519fe2a49abf0cf
hamidghafori2000 <hamidghafori2000@gmail.com>
2025-12-30 12:24:38 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// برای امنیت: چیزی را کش نمی‌کنیم
+self.addEventListener("fetch", (event) => {});
